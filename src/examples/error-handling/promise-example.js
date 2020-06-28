488c71205e9261735dfedfdb55bd7e7e68c8210f
Ben Surgison <ben.surgison@seccl.tech>
2020-06-28 13:19:28 +0100

    Continue with error-handling example

---
+++ src/examples/error-handling/promise-example.js
@@ -0,0 +1,34 @@
+
+const data = {
+  givenName: 'Ben',
+  familyName: 'Surgison'
+}
+
+const getJSON = (error) => {
+  switch (error) {
+    case 'none': return Promise.resolve(JSON.stringify(data))
+    case 'async': return Promise.resolve('*')
+    case 'sync': throw new Error('Sync Error')
+  }
+}
+
+const myRequest = (error) => {
+  try {
+    getJSON(error)
+      .then(result => {
+        // this parse may fail
+        const data = JSON.parse(result)
+        console.log(data)
+      })
+      //  handle asynchronous errors
+      .catch((err) => {
+        console.log(err)
+      })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+myRequest('none')
+// myRequest('sync')
+// myRequest('async')
