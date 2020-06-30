9b2021978249a299d6aa727abcf37e03aa38dda1
Ben Surgison <ben.surgison@seccl.tech>
2020-06-30 09:46:00 +0100

    Make pipeline function even more asynchronous

--- src/examples/keep-it-simple/helper.js
+++ src/examples/keep-it-simple/helper.js
@@ -1,10 +1,18 @@
+const report = async (message) => {
+  console.log(message)
+}
 
 module.exports.pipeLineFunc = async (transactions, amendments) => {
-  return Promise.all(transactions.map(async ({ val, type }) => {
+  const amendedTransactions = await Promise.all(transactions.map(async ({ val, type }) => {
     const amendment = amendments.find(({ type: transType }) => transType === type)
     if (amendment) {
+      const prevVal = val
       val += amendment.adjustment
+      await report(`Adjusted transaction type: "${type}" from ${prevVal} to ${val}`)
     }
+
     return { val, type }
   }))
+
+  return Promise.all(amendedTransactions.map(async (transaction) => ({ ...transaction, status: 'amended' })))
 }
